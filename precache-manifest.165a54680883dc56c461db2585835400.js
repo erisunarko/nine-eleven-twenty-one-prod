8804e4826410fd1e6ab8e6e586a69275cdf9020e
Eri Sunarko <erisunarko@travikr.com>
2021-11-09 17:17:03 +0700

    Commit for Prod

---
+++ precache-manifest.165a54680883dc56c461db2585835400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a274f8e702a6d6a7228763d2b56b4e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9d70b00d59a5470f66d",
+    "url": "/static/css/2.ad81e933.chunk.css"
+  },
+  {
+    "revision": "9e7c6b102be47f248c31",
+    "url": "/static/css/main.44b8ba0d.chunk.css"
+  },
+  {
+    "revision": "c9d70b00d59a5470f66d",
+    "url": "/static/js/2.af49f210.chunk.js"
+  },
+  {
+    "revision": "9e7c6b102be47f248c31",
+    "url": "/static/js/main.84aeb4d9.chunk.js"
+  },
+  {
+    "revision": "e8a7b3468f7ebccf461d",
+    "url": "/static/js/runtime-main.10f3735d.js"
+  }
+]);
